37d5e6d47b62a8f25fa4eb5e32f3bf78e011a90a
LeMufas2a <codage.mufasa@gmail.com>
2021-03-23 12:00:10 +0100

    Exercice 4

---
+++ exercice-4/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="text-center text-danger">
+        <p className='fs-1'>React Bootstrap</p>
+        <i className="fab fa-500px text-danger"></i>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default App;
