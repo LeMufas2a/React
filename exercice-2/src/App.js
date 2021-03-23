c82f2ed2f7a0b1a4bc7d80699e31a96d95a8fdb1
LeMufas2a <codage.mufasa@gmail.com>
2021-03-23 11:59:54 +0100

    Exercice 2

---
+++ exercice-2/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+
+  render() {
+    let name = 'mufasa'
+    return (
+      <div>
+        <h1>
+          {name}
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default App;
